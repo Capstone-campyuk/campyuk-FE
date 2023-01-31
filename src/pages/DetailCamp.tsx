28e708aade2fe9f1ed2ec00273324e01da9107cc
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-01-31 09:36:27 +0700

    Initial: Files pages, component, routes, library

---
+++ src/pages/DetailCamp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DetailCamp() {
+  return (
+    <div>DetailCamp</div>
+  )
+}
+
+export default DetailCamp
---
+++ src/routes/App.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import axios, { AxiosHeaders } from "axios";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import CampList from "../pages/CampList";
+import DetailCamp from "../pages/DetailCamp";
+import Order from "../pages/Order";
+import BookingHistory from "../pages/BookingHistory";
+import BookingDetail from "../pages/BookingDetail";
+import DashboardAdmin from "../pages/DashboardAdmin";
+import DetailCampAdmin from "../pages/DetailCampAdmin";
+import Profile from "../pages/Profile";
+
+function App() {
+  const [cookie, , removeCookie] = useCookies(["token"]);
+  const checkToken = cookie.token;
+
+  axios.interceptors.request.use(function (config) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${cookie.token}`;
+    return config;
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/camplist",
+      element: <CampList />,
+    },
+    {
+      path: "/detail/:id_camp",
+      element: <DetailCamp />,
+    },
+    {
+      path: "/order/:id_order",
+      element: <Order />,
+    },
+    {
+      path: "/booking-history",
+      element: checkToken ? <BookingHistory /> : <Login />,
+    },
+    {
+      path: "/booking-detail/:id_booking",
+      element: checkToken ? <BookingDetail /> : <Login />,
+    },
+    {
+      path: "/dashboard-admin",
+      element: checkToken ? <DashboardAdmin /> : <Login />,
+    },
+    {
+      path: "/detail-camp-admin/:id_camp",
+      element: checkToken ? <DetailCampAdmin /> : <Login />,
+    },
+    {
+      path: "/profile/:id-username",
+      element: checkToken ? <Profile /> : <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
