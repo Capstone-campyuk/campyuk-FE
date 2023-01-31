67f00bb5890c6b334662aa825a0127f5cbcfe4d5
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-02-01 00:49:41 +0700

    Feat: Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-function Footer() {
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-primary flex justify-center items-center text-white h-14">
+      <p>© 2023 Campyuk Copyright. All Rights Reserved.</p>
+    </div>
+  );
 }
-
-export default Footer
